132f58fe86bf0b68e283895974f5efcd468af316
JoaoSchulz <joaoguilherme.da@gmail.com>
2022-07-04 07:43:40 -0300

    formulario com viacep

--- js/anunciar.js
+++ js/anunciar.js
@@ -5,6 +5,10 @@ const opcao = document.getElementById('opcaoAnunciar')
 const email = document.getElementById('emailAnunciar')
 const telefone = document.getElementById('telefoneAnunciar')
 const cep = document.getElementById('cepAnunciar')
+const rua = document.querySelector('#rua')
+const bairro = document.querySelector('#bairro')
+const cidade = document.querySelector('#cidade')
+
 
 titulo.addEventListener('blur', function(){
 
@@ -63,6 +67,57 @@ telefone.addEventListener('blur', function(){
         setSuccessFor(telefone);
     }
 });
+
+cep.addEventListener('blur', function(e){
+
+    let cep = e.target.value;
+    limparInput();
+    let script = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
+    addLoading();
+    document.body.appendChild(script);
+    if(cep === ''){
+        removLoading();
+    }
+});
+
+function popularForm(resposta){
+
+    if("erro" in resposta){
+        setErrorFor(cep,'CEP não encontrado')
+        removLoading();
+    }else{
+        setSuccessFor(cep)
+        removLoading();
+    }
+
+    rua.value = resposta.logradouro;
+    bairro.value = resposta.bairro;
+    cidade.value = resposta.localidade;
+}
+
+
+function addLoading(){
+    var recebeSpan = document.querySelectorAll('.loading');
+    for(var i = 0; i < recebeSpan.length; i++) {
+        recebeSpan[i].classList.add('loadingVisible');
+        
+    }
+}
+
+function removLoading(){
+    var recebeSpan = document.querySelectorAll('.loading');
+    for(var i = 0; i < recebeSpan.length; i++) {
+    recebeSpan[i].classList.remove('loadingVisible');
+    }
+}
+
+function limparInput(){
+
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+}
 
 function setErrorFor(input, message) {
     const controleFormulario = input.parentElement;
